931941ada64e1c52e14dec585b598afae5b1b907
marcolapcx <168039947+marcolapcx@users.noreply.github.com>
2024-09-16 21:36:19 -0300

    While

---
+++ src/DowhileExemplo.java
@@ -0,0 +1,2 @@
+public class DowhileExemplo {
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+
+        int valor = 0;
+        int somaTotal = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+
+        while (valor != 99){
+            /*System.out.println(valor);
+            valor= valor+ 1; // valor++ */
+
+            System.out.println("Infome o valor");
+            System.out.println("Infome 99 para sair");
+            valor = sc.nextInt();
+
+            somaTotal += valor;
+
+        }
+
+        System.out.println("Total é " + somaTotal);
+
     }
+
 }
